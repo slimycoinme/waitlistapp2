07f76978b53bf89b616d20e3c5a28cae27789271
slimycoinme <info@mijnders.nl>
2024-12-17 17:26:35 +0700

    Updated build configuration and MIME types

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          firebase: ['firebase/app', 'firebase/firestore']
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
       },
     },
   },
-  base: '/',
   server: {
     port: 5173,
   },
